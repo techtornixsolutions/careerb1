08e9cbe51bc3f01a3814fd938eaa1cca6e826a4e
muhammadbahawal <muhammadbahawalsher@gmail.com>
2025-07-14 16:38:30 +0500

    javascript start

---
+++ new/javascript/index.js
@@ -0,0 +1,20 @@
+let Name = "Ali";
+var age = 20;
+
+console.log(age);
+let a = 10;
+let b = "ali";
+var age = 30;
+
+console.log(age);
+
+
+// string =""
+// int =132
+// Boolean = true
+// Float = 2.4
+// Array= [1,3,5,"hello",true]
+// Object={
+//     Name:"ali";
+// }
+
